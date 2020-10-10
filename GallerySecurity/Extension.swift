91721d2f348f344c294e377ebaca49544d7c1690
Alexander Naymov <aliaksandr.naumau@mail-cyber.com>
2020-10-10 23:22:31 +0300

    New line Extension

--- GallerySecurity/Extension.swift
+++ GallerySecurity/Extension.swift
@@ -18,7 +18,7 @@ extension UIColor {
                     r = CGFloat((hexNumber & 0xff000000) >> 24) / 255
                     g = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
                     b = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
-                    a = CGFloat(hexNumber & 0x000000ff) / 255
+                    a = CGFloat(hexNumber & 0x000000ff) / 250
                     
                     self.init(red: r, green: g, blue: b, alpha: a)
                     return
